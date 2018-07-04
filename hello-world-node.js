5e516beb02b3be6a592cc3bcdeb32057d68bccaf
BEMELON <hgdkim2@naver.com>
2018-07-04 15:58:46 +0900

    hello-world! in node

---
+++ hello-world-node.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function handler(req, res){
+    res.writeHead(200, {'Content-Tpye': 'text/plain'});
+    res.end("hello world\n");
+}).listen(1337);
+console.log("Server running at https://127.0.0.1");
+
