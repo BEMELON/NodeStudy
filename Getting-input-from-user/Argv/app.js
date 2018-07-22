e1376089a118194dd30d0a0799c54a8727f9f59a
BEMELON <hgdkim2@naver.com>
2018-07-22 12:18:37 +0900

    Update Yarg sources.

--- Getting-input-from-user/Argv/app.js
+++ Getting-input-from-user/Argv/app.js
@@ -4,9 +4,33 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const yargs = require('yargs');
-console.log('Yargs', argv);
+//console.log('Yargs', argv);
+const titleOption = {
+    describe : `Title of note`,
+    demand : true,
+    alias : `t`
+};
 
-const argv = yargs.argv;
+const bodyOption = {
+    describe : `Body of note`,
+    demand : true,
+    alias : `b`
+};
+const argv = yargs
+    .command('add', 'Add a new note',{
+        title: titleOption,
+        body: bodyOption
+    })
+    .command('list", "list all notes')
+    .command(`remove`, `Remove a note`, {
+        title: titleOption
+    })
+    .command(`read`, `Read a note`, {
+        title: titleOption
+    })
+    .help() // shows what other options is exist
+    .argv;
+var command = argv._[0];
 console.log('Process', process.argv);
 
 var command = process.argv[2];
--- Getting-input-from-user/Save-Load/notes.js
+++ Getting-input-from-user/Save-Load/notes.js
@@ -36,6 +36,7 @@ var addNote = (title, body) => {
 
 var getAll = () => {
     console.log("Getting all notes");
+    return fetchNotes();
 };
 
 var getNote = (title) => {
