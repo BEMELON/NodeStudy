46a6c1267c109c5f4ed05a392b7c8a0137c98f20
BEMELON <BEMELON@github.com>
2018-07-24 00:45:24 +0900

    Fix API-Call error

--- Weather-app-Async/WeatherApplication/app.js
+++ Weather-app-Async/WeatherApplication/app.js
@@ -1,7 +1,7 @@
 const request = require(`request`);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB8p2OTXbV5UpZV4sdQyOuwSsCIT58jD-A`,
     json: true
 }, (error, response, body) => {
     console.log(body);
