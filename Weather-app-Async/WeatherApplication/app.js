afefd70c1f32a85f509f24876d8e41bc725d1dfe
BEMELON <hgdkim2@naver.com>
2018-07-25 20:49:54 +0900

    Update app.js

--- Weather-app-Async/WeatherApplication/app.js
+++ Weather-app-Async/WeatherApplication/app.js
@@ -13,13 +13,22 @@ const argv = yargs
     .alias(`help`, `h`)
     .argv;
 
-console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 
+// error -> Machine error (connection error)
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB8p2OTXbV5UpZV4sdQyOuwSsCIT58jD-A`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyB8p2OTXbV5UpZV4sdQyOuwSsCIT58jD-A`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log(`Unable to connect to Google-servers.`);   
+    } else if(body.status === `ZERO_RESULTS`) {
+        console.log(`Unable to find that address.`);
+    } else if(body.status === `OK`) {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log(body.status);
+    }
 })
