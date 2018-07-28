244ff6ac997d39f95537cf97237ca3feb814e199
BEMELON <hgdkim2@naver.com>
2018-07-28 18:32:56 +0900

    Add Middleware in server.js

--- Express-Webserver/server.js
+++ Express-Webserver/server.js
@@ -1,28 +1,51 @@
 const express =require(`express`);
-
+const hbs = require(`hbs`);
+const fs = require(`fs`);
 var app = express();
 
-// dirname -> EXPRESS_WEBSERVEr 까지의 경로
+hbs.registerPartials(__dirname + `/views/partials`);
+hbs.registerHelper(`getCurrentYear`,() => new Date().getFullYear()); // Similar with Macro function
+hbs.registerHelper(`screamIt`, (text) => text.toUpperCase())
+
+
+app.set(`view engine`, `hbs`);
+// dirname -> EXPRESS_WEBSERVER 까지의 절대경로
+app.use((req, res, next) => {
+    // when next() occurs. Application continue to run 
+    var now = new Date().toString(); 
+    // method -> get, post 
+    // url -> /, /about, /help ...
+    var log = `${now} : ${req.method} ${req.url}`;
+    console.log(log);
+   
+    fs.appendFile(`server.log`, log + `\n`, (err) => {
+        if (err) {
+            console.log(`Unable to append to server log`);
+        } 
+    });
+    next();
+});
+
 app.use(express.static(__dirname + `/public`));
 // request >> header, bodty request, All of Body
 // What will you  respon go
 app.get(`/`, (req, res) => {
-    // res.send(`<h1>Hello Express!</h1>`);
-    res.send({
+    res.render(`home.hbs`, {
         name: `Belon`,
         likes: [
             `Biking`,
             `Gaming`,
             `Sleeping`,
-            `Coding`
-        ]
-    });
+        ],
+        welcomeMessage: `Welcome to my Page!`
+    })
 });
 
 app.get(`/about`, (req, res) => {
-    res.send(`About Page`);
+    res.render(`about.hbs`, {
+        pageTitle : `About Page`,
+    });
 });
-// /bad - send back json with errorMessage
 
 app.get('/bad', (req, res) => {
     res.send({
